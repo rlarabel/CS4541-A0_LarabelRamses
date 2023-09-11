490b39e83babaa1eae94c972991bbc85558fac13
ramses <rlarabel@gmail.com>
2023-09-11 17:03:13 -0400

    hello, world

---
+++ README.txt
@@ -0,0 +1,13 @@
+Date: 9/11/2023  
+Class: CS4541
+Assignment: Assignment 0  
+Author(s): Ramses Larabel
+
+#Description
+First basic assignment for Computer Systems Fundamental
+
+#Inputs / Outputs
+##Command Line Input
+```./a.out
+##Command Line Output
+```Hello, World!
---
+++ hello.c
@@ -0,0 +1,13 @@
+/*********************************************
+    Date: 9/11/2024
+    Class: CS4541
+    Assignment: Assignment 0
+    Author(s): Ramses Larabel
+*******************************************/
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 1;
+}
+
